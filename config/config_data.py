113a963b5d784f1951687eb50e749e0636c5523e
NezBut <xyi12312354@mail.ru>
2023-12-12 00:32:17 +0700

    config file

--- config/config_data.py
+++ config/config_data.py
@@ -7,27 +7,43 @@ load_dotenv()
 @dataclass
 class TelegramBot:
     bot_token: str
+    bot_logs: bool
 
 @dataclass
 class DataBase:
     sql_alchemy_url: str
+    db_logs: bool
 
 @dataclass
 class ConfigData:
     tg_bot: TelegramBot
     database: DataBase
 
-def load_config_data() -> ConfigData:
+def _get_a_choice_of_parameter(parametr: str) -> bool:
+    menu = getenv(parametr.strip().upper()).upper()
+    menu_choose = menu if menu in ('YES', 'NO') else 'NO'
+
+    match menu_choose:
+
+        case "YES":
+            return True
+
+        case "NO":
+            return False
+
+def _load_config_data() -> ConfigData:
     return ConfigData(
         tg_bot=TelegramBot(
-            bot_token=getenv('BOT_TOKEN')
+            bot_token=getenv('BOT_TOKEN'),
+            bot_logs=_get_a_choice_of_parameter(parametr='BOT_LOGS')
         ),
         database=DataBase(
-            sql_alchemy_url=getenv('SQLALCHEMY_URL')
+            sql_alchemy_url=getenv('SQLALCHEMY_URL'),
+            db_logs=_get_a_choice_of_parameter(parametr='DATABASE_LOGS')
         )
     )
 
-Config = load_config_data()
+Config = _load_config_data()
 
 __all__ = (
     'Config',
